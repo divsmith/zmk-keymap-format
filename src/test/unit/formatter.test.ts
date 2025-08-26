7b53a150860495ebd09446a0e86c953dff8d3ad3
Parker Smith <parker@parkersmith.us>
2025-08-25 23:14:45 -0600

    failing test

--- src/test/unit/formatter.test.ts
+++ src/test/unit/formatter.test.ts
@@ -3,6 +3,7 @@ import * as assert from 'assert';
 const formatter = require('../../../out/formatter');
 
 describe('Formatter Tests', () => {
+
   it('simple 2x2 formatting', () => {
     const input = `
         #include <dt-bindings/zmk/keys.h>
@@ -124,6 +125,47 @@ describe('Formatter Tests', () => {
                                 | &kp W |
                                         | &kp E |
                         | &kp R |
+                    >;
+                };
+            };
+        };`;
+    
+    assert.strictEqual(formatter.formatDocument(input), expected);
+  });
+
+  it('2x2 with wider columns', () => {
+    const input = `
+        #include <dt-bindings/zmk/keys.h>
+
+        // Keymap Template
+        // | * | * |
+        // | * | * |
+
+        / {
+            keymap {
+                compatible = "zmk,keymap";
+                layer_0 {
+                    bindings = <
+                        &abc Q &kp W &kp E &kp R
+                    >;
+                };
+            };
+        };`;
+
+    const expected = `
+        #include <dt-bindings/zmk/keys.h>
+
+        // Keymap Template
+        // | * | * |
+        // | * | * |
+
+        / {
+            keymap {
+                compatible = "zmk,keymap";
+                layer_0 {
+                    bindings = <
+                        | &abc Q | &kp W |
+                        | &kp  E | &kp R |
                     >;
                 };
             };
